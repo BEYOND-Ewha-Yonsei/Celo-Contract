d9380adb7b27dfa4ef9d596a9a2966eab2b287ae
seungwon2 <dandelionsw2@ewhain.net>
2021-03-30 17:24:30 +0900

    :sparkles: feat : add Token Name

--- contracts/MarkAtToken.sol
+++ contracts/MarkAtToken.sol
@@ -1,8 +1,8 @@
-pragma solidity ^0.8.0;
+pragma solidity >=0.5.0 <0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 
-contract MyCollectible is ERC721 {
+contract MarkAtToken is ERC721 {
     constructor(string _name, string _symbol)
         public
         ERC721Token(_name, _symbol)
